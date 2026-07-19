f42c3dce6cfe1020a0cf4dd649b41c50e3cb66c8
JChilTG <james.chilcott@icloud.com>
2026-07-19 11:48:48 +0000

    Test

--- synapse/models/staging/stg_d365_account_snapshots.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-select
-    core.*,
-    {{ hash_all_except(
-        ref('stg_d365_account_snapshots_core'),
-        var('scd2_hash_exclude_columns')
-    ) }} as attribute_hash
-from {{ ref('stg_d365_account_snapshots_core') }} as core
--- synapse/tests/assert_d365_snapshot_grain.sql
+++
@@ -1,9 +0,0 @@
--- Each account should appear at most once per extract date.
--- Valid T-SQL as-is.
-select
-    snapshot_date,
-    account_id,
-    count(*) as row_count
-from {{ ref('raw_d365_account_snapshots') }}
-group by snapshot_date, account_id
-having count(*) > 1
